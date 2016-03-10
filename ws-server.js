1f4cbc331ddd4603253ec5bfe0c494e2a46ee3dc
Paul Frazee <pfrazee@gmail.com>
2016-03-10 13:40:26 -0600

    add prompt-pages/publish for userland publish() calls

--- ws-server.js
+++ ws-server.js
@@ -2,6 +2,15 @@ var muxrpc = require('muxrpc')
 var pull = require('pull-stream')
 var Serializer = require('pull-serializer')
 
+// platform-specific opener
+var open
+if (process.versions['electron']) {
+  var shell = require('electron').shell
+  open = shell.openExternal.bind(shell)
+} else {
+  open = require('open')
+}
+
 module.exports.Trusted = function (sbot, config) {
   var manifest = config.getTrustedManifest(sbot)
   var perms = config.getTrustedPerms()
@@ -17,9 +26,17 @@ module.exports.Userland = function (sbot, config) {
   var manifest = config.getUserlandManifest(sbot)
   var perms = config.getUserlandPerms()
 
+  // special api behaviors
+  var api = Object.assign({}, sbot, {
+    publish: function (msg, cb) {
+      open('http://localhost:7777/prompt-pages/publish.html#'+encodeURIComponent(JSON.stringify(msg)))
+      cb()
+    }
+  })
+
   return function (stream) {
     // create rpc stream
-    var rpc = muxrpc({}, manifest, serialize)(sbot, perms)
+    var rpc = muxrpc({}, manifest, serialize)(api, perms)
     pull(stream, rpc.createStream(), stream)
   }
 }
